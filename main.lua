a45112edc9c705c5df73937dacc3acb4dd0143b6
Andrés Gustavo <gd_andres98@hotmail.com>
2022-12-22 03:26:43 -0500

    Pong mockup

--- main.lua
+++ main.lua
@@ -7,7 +7,10 @@ VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
 function love.load()
-    love.graphics.setDefaultFilter('nearest','nearest')
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    smallFont = love.graphics.newFont('font.ttf', 8)
+    love.graphics.setFont(smallFont)
 
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
@@ -19,13 +22,20 @@ end
 function love.draw()
     push:apply('start')
 
+    love.graphics.clear(40 / 255, 45 / 255, 52 / 255, 255 / 255)
+
     love.graphics.printf(
         'Hello World!',
         0,
-        VIRTUAL_HEIGHT / 2 - 6,
+        20,
         VIRTUAL_WIDTH,
         'center'
     )
+
+    love.graphics.rectangle('fill', 10, 30, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH/2 - 2, VIRTUAL_HEIGHT/2 - 2, 4, 4)
+
 
     push:apply('end')
 end
@@ -33,5 +43,5 @@ end
 function love.keypressed(key)
     if key == 'escape' then
         love.event.quit()
-    end 
+    end
 end
