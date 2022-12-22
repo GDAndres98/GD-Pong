848b4e9e9affee62ee100051534540f0161b10cf
Andrés Gustavo <gd_andres98@hotmail.com>
2022-12-22 03:16:21 -0500

    Low res effect with push

--- main.lua
+++ main.lua
@@ -1,8 +1,15 @@
+push = require 'push'
+
 WINDOW_WIDTH = 1920
 WINDOW_HEIGHT = 1080
 
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
 function love.load()
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT,{
+    love.graphics.setDefaultFilter('nearest','nearest')
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
         vsync = true
@@ -10,11 +17,21 @@ function love.load()
 end
 
 function love.draw()
+    push:apply('start')
+
     love.graphics.printf(
         'Hello World!',
         0,
-        WINDOW_HEIGHT/2-6,
-        WINDOW_WIDTH,
+        VIRTUAL_HEIGHT / 2 - 6,
+        VIRTUAL_WIDTH,
         'center'
     )
+
+    push:apply('end')
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end 
 end
